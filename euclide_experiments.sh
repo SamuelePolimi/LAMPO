0f0cf163a5c39bb46dcc7c7466a59eb1d449b914
Samuele <samuele@marconi.ias.informatik.tu-darmstadt.de>
2020-09-21 22:46:03 +0200

    new experiment for euclide

--- euclide_experiments.sh
+++ euclide_experiments.sh
@@ -2,19 +2,38 @@
 
 # Imitation learning size
 
-python experiment_organizer.py il_size_1 -n 10 --n_evaluations 500 --imitation_learning 100 --batch_size 500 -z --max_iter 10 --dense_reward
-python experiment_organizer.py il_size_2 -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 500 -z --max_iter 10 --dense_reward
-python experiment_organizer.py il_size_3 -n 10 --n_evaluations 500 --imitation_learning 500 --batch_size 500 -z --max_iter 10 --dense_reward
-python experiment_organizer.py il_size_4 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 --dense_reward
+python experiment_organizer.py il_size_1_dense -n 10 --n_evaluations 500 --imitation_learning 100 --batch_size 500 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py il_size_2_dense -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 500 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py il_size_3_dense -n 10 --n_evaluations 500 --imitation_learning 500 --batch_size 500 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py il_size_4_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 --dense_reward -c -1.
 
 # Reinforcement learning
 
-python experiment_organizer.py rl_size_1 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 20 -z --max_iter 10 --dense_reward
-python experiment_organizer.py rl_size_2 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 50 -z --max_iter 10 --dense_reward
-python experiment_organizer.py rl_size_3 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 100 -z --max_iter 10 --dense_reward
-python experiment_organizer.py rl_size_4 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 200 -z --max_iter 10 --dense_reward
-python experiment_organizer.py rl_size_5 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 --dense_reward
+python experiment_organizer.py rl_size_1_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 20 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py rl_size_2_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 50 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py rl_size_3_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 100 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py rl_size_4_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 200 -z --max_iter 10 --dense_reward -c -1.
+python experiment_organizer.py rl_size_5_dense -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 --dense_reward -c -1.
 
 # Minimal
 
-python experiment_organizer.py small -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 20 -z --max_iter 10 --dense_reward
+python experiment_organizer.py small_dense -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 20 -z --max_iter 10 --dense_reward -c -1.
+
+# Imitation learning size
+
+python experiment_organizer.py il_size_1 -n 10 --n_evaluations 500 --imitation_learning 100 --batch_size 500 -z --max_iter 10 -c -1.
+python experiment_organizer.py il_size_2 -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 500 -z --max_iter 10 -c -1.
+python experiment_organizer.py il_size_3 -n 10 --n_evaluations 500 --imitation_learning 500 --batch_size 500 -z --max_iter 10 -c -1.
+python experiment_organizer.py il_size_4 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 -c -1.
+
+# Reinforcement learning
+
+python experiment_organizer.py rl_size_1 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 20 -z --max_iter 10 -c -1.
+python experiment_organizer.py rl_size_2 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 50 -z --max_iter 10 -c -1.
+python experiment_organizer.py rl_size_3 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 100 -z --max_iter 10 -c -1.
+python experiment_organizer.py rl_size_4 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 200 -z --max_iter 10 -c -1.
+python experiment_organizer.py rl_size_5 -n 10 --n_evaluations 500 --imitation_learning 1000 --batch_size 500 -z --max_iter 10 -c -1.
+
+# Minimal
+
+python experiment_organizer.py small -n 10 --n_evaluations 500 --imitation_learning 200 --batch_size 20 -z --max_iter 10 -c -1.
