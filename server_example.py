c0a5ab93cbba1de3b5f2fb8326bdf24f38b29bbd
tosatto <samuele@robot-learning.de>
2020-09-23 22:03:21 +0200

    server example is actually the current client in the lab

--- server_example.py
+++ server_example.py
@@ -1,4 +1,13 @@
-from core.lab_connection import TCPServerExample
-from core.config import config
-server = TCPServerExample(5050, 3)
-server.run()
+from core.lab_connection import TCPClientExample
+from sshtunnel import SSHTunnelForwarder
+
+
+with SSHTunnelForwarder(
+        ("kay.ias.informatik.tu-darmstadt.de", 22),
+        ssh_username="tosatto",
+        ssh_pkey="/home/samuele/.ssh/id_rsa_tosatto",
+        remote_bind_address=("130.83.164.60", 5056),
+        local_bind_address=("127.0.0.1", 5050)
+) as tunnel:
+    client = TCPClientExample("127.0.0.1", 5050, 3)
+    client.run()
