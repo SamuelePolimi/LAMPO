a2d55592d933a38f4d0d08b31970fef7c0dd5e7e
Samuele <samuele@marconi.ias.informatik.tu-darmstadt.de>
2020-10-12 01:02:21 +0200

    drawer for euclide

--- experiment_drawer_batch.sh
+++ experiment_drawer_batch.sh
@@ -1,4 +1,4 @@
-for kl in 0.1 0.2 0.5 1.0 2.0:
+for kl in 0.5
 do
-  python experiment_organizer.py close_drawer_${kl} --task_name close_drawer -n 5 --n_evaluations 500 --imitation_learning 200 --batch_size 100 -z --max_iter 10 -c -1 --id 0 --il_noise 0.0 --context_reg -1.0  --kl_bound ${kl}
+  python experiment_organizer.py close_drawer_${kl} --task_name close_drawer -n 5 --n_evaluations 500 --imitation_learning 200 --batch_size 100 -z --max_iter 5 -c -1 --il_noise 0.03  --kl_bound ${kl} --forward
 done
